8f40194c95f3eb065a40452bf7e6004c7d164499
William Li <email@williamli.dev>
2023-03-08 13:38:04 +0800

    👋🏽

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,14 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>Here are some additional changes to this page.</p>
-          <p>Here are some additional changes to this page.</p>
-          <p>Here are some additional changes to this page.</p>
-          <p>Here are some additional changes to this page.</p>
-          <p>Here are some additional changes to this page.</p>
-          <p>Here are some additional changes to this page.</p>
-          <p>Here are some additional changes to this page.</p>
-          <p>Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
+          <p>👋🏽 Here are some additional changes to this page.</p>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
