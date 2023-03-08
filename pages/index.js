63451caef376c165a2d572d32b135da928029697
William Li <email@williamli.dev>
2023-03-08 19:18:19 +0800

    what?

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>Here are some additional changes to this page.</p>
+          <p>WHAT???</p>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
